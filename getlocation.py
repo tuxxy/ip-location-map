9faf604b2f6ae7466e6ae07a3e1f8111005e7174
Tux <alphanani@gmail.com>
2015-04-01 01:04:56 -0700

    Performance fixes and make code more readable

--- getlocation.py
+++ getlocation.py
@@ -3,12 +3,14 @@ from pyipinfodb import pyipinfodb
 
 ip_lookup = pyipinfodb.IPInfo('API_KEY_HERE')
 
+coord_list = [] # List of tuples containing coordinates as (Longitude, Latitude)
+with open('ips.txt','r') as ip_file:
+    for ip in ip_file:
+        print('Getting location for {}'.format(ip))
+        ip_data=ip_lookup.get_city(ip)
+        coord_list.append((ip_data['longitude'], ip_data['latitude']))
 
-with open('ips.txt','r') as f:
-	for line in f.readlines():
-	        print('Getting location for {}'.format(line))
-		req=ip_lookup.get_city(line)
-		time.sleep(0.5)
-		with open('geo.txt', 'ab') as geo:
-			geo.write('{},{}\n'.format(req['longitude'], req['latitude']))
-
+with open('geo.txt', 'w') as geo_file:
+    for coordinates in coord_list:
+        ip_longitude, ip_latitude = coordinates
+        geo_file.write('{},{}\n'.format(ip_longitude, ip_latitude))
