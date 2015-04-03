427c14c8f4e27aca389bd2ff0d43b0093c5405f4
Lucretiel <Lucretiel@users.noreply.github.com>
2015-04-02 20:07:53 -0400

    getlocation simplifications
    - Now uses lru_cache for ip caching
    - Now uses stdin and stdout rather than fixed filenames. Can be used in a pipeline
    - Now respects `__name__ == "__main__"`
    - Now uses print instead of write

--- getlocation.py
+++ getlocation.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 from pyipinfodb import pyipinfodb
+from sys import stdin, stderr
+from functools import lru_cache
 
-ip_lookup = pyipinfodb.IPInfo('API_KEY_HERE')
+@lru_cache(maxsize=None)
+def get_location(ip):
+    print('Getting location for', ip, file=stderr)
+    ip_data = ip_lookup.get_city(ip)
+    yield ip_data['longitude'], ip_data['latitude']
 
-coord_list = [] # List of tuples containing coordinates as (Longitude, Latitude)
-cache = {} # Dict containing data from IPs that have already been requested
-           # from the API
-with open('ips.txt','r') as ip_file:
-    for ip in ip_file:
-        print('Getting location for {}'.format(ip))
-        if ip not in cache:
-            ip_data=ip_lookup.get_city(ip)
-            cache[ip] = ip_data
-        else:
-            ip_data = cache[ip]
-        coord_list.append((ip_data['longitude'], ip_data['latitude']))
+def main():
+    for ip in stdin:
+        print(*get_location(ip), sep=',')
 
-with open('geo.txt', 'w') as geo_file:
-    for coordinates in coord_list:
-        ip_longitude, ip_latitude = coordinates
-        geo_file.write('{},{}\n'.format(ip_longitude, ip_latitude))
+if __name__ == '__main__':
+    main()
